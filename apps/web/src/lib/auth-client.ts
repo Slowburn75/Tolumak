edd9f74be6a8fa45dfa4a285ae9f513e07fda057
Slowburn75 <16010211011a@gmail.com>
2026-01-22 10:56:17 +0100

    initial commit

---
+++ apps/web/src/lib/auth-client.ts
@@ -0,0 +1,6 @@
+import { env } from "@Tolumak/env/web";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: env.NEXT_PUBLIC_SERVER_URL,
+});
